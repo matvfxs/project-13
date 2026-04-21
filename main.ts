a577f40627346894be04b7bac289191835e6d7e2
matvfxs <verschaevemats@gmail.com>
2026-04-21 12:32:18 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 led.enable(false)
 basic.forever(function () {
-    pins.digitalWritePin(DigitalPin.P3, 1)
+    pins.digitalWritePin(DigitalPin.P4, 1)
+    pins.digitalWritePin(DigitalPin.P3, 0)
     pins.digitalWritePin(DigitalPin.P7, 0)
 })
